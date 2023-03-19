df1d7faa2590f56b11c9cccb548029a269bced02
Julia Czosnek <100991936+Julka235@users.noreply.github.com>
2023-03-19 19:27:20 +0100

    Task 4 (#5)
    * added spring and REST controller
    
    * fixed checkstyle error in InvoiceController
    
    * renamed App to InvoiceApplication

--- src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
+++ src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
@@ -57,37 +57,43 @@ public class FileBasedDatabase implements Database {
   }
 
   @Override
-  public void update(int id, Invoice updatedInvoice) {
+  public Optional<Invoice> update(int id, Invoice updatedInvoice) {
     try {
       List<String> allInvoices = fileService.readAllLines(databasePath);
-      var listWithoutInvoiceWithGivenId = allInvoices
+      var invoicesWithoutInvoiceWithGivenId = allInvoices
           .stream()
           .filter(line -> !containsId(line, id))
           .collect(Collectors.toList());
 
-      if (allInvoices.size() == listWithoutInvoiceWithGivenId.size()) {
-        throw new IllegalArgumentException("Id " + id + " does not exist");
-      }
-
       updatedInvoice.setId(id);
-      listWithoutInvoiceWithGivenId.add(jsonService.toJson(updatedInvoice));
+      invoicesWithoutInvoiceWithGivenId.add(jsonService.toJson(updatedInvoice));
 
-      fileService.writeLinesToFile(databasePath, listWithoutInvoiceWithGivenId);
+      fileService.writeLinesToFile(databasePath, invoicesWithoutInvoiceWithGivenId);
+
+      allInvoices.removeAll(invoicesWithoutInvoiceWithGivenId);
+      return allInvoices.isEmpty() ? Optional.empty() : Optional.of(jsonService.toObject(allInvoices.get(0), Invoice.class));
 
     } catch (IOException ex) {
       throw new RuntimeException("Failed to update invoice with id: " + id, ex);
     }
+
   }
 
   @Override
-  public void delete(int id) {
+  public Optional<Invoice> delete(int id) {
     try {
-      var updatedList = fileService.readAllLines(databasePath)
+      var allInvoices = fileService.readAllLines(databasePath);
+
+      var invoicesExceptDeleted = allInvoices
           .stream()
           .filter(line -> !containsId(line, id))
           .collect(Collectors.toList());
 
-      fileService.writeLinesToFile(databasePath, updatedList);
+      fileService.writeLinesToFile(databasePath, invoicesExceptDeleted);
+
+      allInvoices.removeAll(invoicesExceptDeleted);
+
+      return allInvoices.isEmpty() ? Optional.empty() : Optional.of(jsonService.toObject(allInvoices.get(0), Invoice.class));
 
     } catch (IOException ex) {
       throw new RuntimeException("Failed to delete invoice with id: " + id, ex);
