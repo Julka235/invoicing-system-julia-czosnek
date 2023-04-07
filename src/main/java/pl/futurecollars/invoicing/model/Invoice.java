258ee3d5889e62f90a040a377cd7ef0fd7d77ef1
Julia Czosnek <julia.cz13@o2.pl>
2023-04-07 19:32:49 +0200

    Task 9

--- src/main/java/pl/futurecollars/invoicing/model/Invoice.java
+++ src/main/java/pl/futurecollars/invoicing/model/Invoice.java
@@ -4,10 +4,12 @@ import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Invoice {
